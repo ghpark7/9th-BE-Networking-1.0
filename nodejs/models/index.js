add02499dee5322eb4014ab868eaddfcde5b0100
Geonhwi Park <geonhwipark715@gmail.com>
2024-05-09 19:12:19 +0900

    Update index.js
    simple mod

--- nodejs/models/index.js
+++ nodejs/models/index.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+const sequelize = require('sequelize');
 const config = require('../config/config.json')['development'];
 const Property = require('./property');
 const XLSX = require('xlsx');
