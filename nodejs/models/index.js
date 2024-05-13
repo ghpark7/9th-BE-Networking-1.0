5d2ec0a4b3731f4cdd33f287ec89bea7aab07db0
Geonhwi Park <geonhwipark715@gmail.com>
2024-05-13 14:53:57 +0900

    db 연동 안되는 문제 해결
    db 연동 안되는 문제 해결

--- nodejs/models/index.js
+++ nodejs/models/index.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize');
-const config = require('../config/config.json')['development'];
 const Property = require('./property');
-const XLSX = require('xlsx');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
 
+const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     dialect: config.dialect
@@ -10,24 +11,7 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
 
 Property.initiate(sequelize);
 
-// 엑셀 파일 읽기
-const workbook = XLSX.readFile('./매물_정보.xlsx');
-const sheet_name_list = workbook.SheetNames;
-const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-
-// 데이터베이스에 데이터 저장
-data.forEach(async (row) => {
-  await Property.create({
-      postalCode: row['우편번호'],
-      roadAddress: `${row['시도']} ${row['시군구']} ${row['도로명']} ${row['건물번호 본번']}-${row['건물번호 부번']}`,
-      jibunAddress: `${row['시도']} ${row['시군구']} ${row['법정동명']} ${row['지번본번']}-${row['지번부번']}`
-  });
-});
-
-const db = {
-    sequelize,
-    Sequelize,
-    Property
-};
+db.sequelize = sequelize;
+db.Property = Property;
 
 module.exports = db;
--- nodejs/models/property.js
+++ nodejs/models/property.js
@@ -1,36 +1,36 @@
-const Sequelize = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 
-class Property extends Sequelize.Model {
+class Property extends Model {
     static initiate(sequelize) {
-        Property.init({
+        this.init({
             id: {
-                type: Sequelize.BIGINT,
+                type: DataTypes.BIGINT,
                 autoIncrement: true,
                 primaryKey: true
             },
             postalCode: {
-                type: Sequelize.STRING(50),
+                type: DataTypes.STRING(50),
                 allowNull: false,
             },
             roadAddress: {
-                type: Sequelize.STRING(100),
+                type: DataTypes.STRING(100),
                 allowNull: true,
             },
             jibunAddress: {
-                type: Sequelize.STRING(100),
+                type: DataTypes.STRING(100),
                 allowNull: true,
             }
         }, {
-                sequelize,
-                timestamps: true,
-                underscored: false,
-                modelName: 'Property',
-                tableName: 'property',
-                paranoid: true,
-                charset: 'utf8',
-                collate: 'utf8_general_ci',
+            sequelize,
+            modelName: 'Property',
+            tableName: 'properties',
+            timestamps: true,
+            underscored: false,
+            paranoid: true,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
         });
     }
-};
+}
 
 module.exports = Property;
